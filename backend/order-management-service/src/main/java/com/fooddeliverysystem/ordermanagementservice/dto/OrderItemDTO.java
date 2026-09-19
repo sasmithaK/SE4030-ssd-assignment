c681364844fe8e84b269b33eb21b6583ca1dbd81
Yoshini Lakna <yoshinilakna@gmail.com>
2026-09-19 18:22:15 +0530

    [VULN-6] fix: enforce bean validation for OrderController and Order DTOs
    Fixed A03 Injection vulnerability by adding @Valid to OrderController endpoints and missing validation constraints like @NotEmpty and @NotBlank to OrderDTO and OrderItemDTO.

--- backend/order-management-service/src/main/java/com/fooddeliverysystem/ordermanagementservice/dto/OrderItemDTO.java
+++ backend/order-management-service/src/main/java/com/fooddeliverysystem/ordermanagementservice/dto/OrderItemDTO.java
@@ -1,5 +1,7 @@
 package com.fooddeliverysystem.ordermanagementservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -8,9 +10,18 @@ import lombok.*;
 @Builder
 public class OrderItemDTO {
     private Long id;
+
+    @NotBlank(message = "Item name is required")
     private String name;
+
+    @NotBlank(message = "Price is required")
     private String price;
+
+    @NotBlank(message = "Portion is required")
     private String portion;
+
     private String specialInstructions;
+
+    @NotNull(message = "Quantity is required")
     private Integer qty;
 }
